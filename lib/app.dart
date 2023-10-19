e7d904ae04ddba06f739316b0a21b6422834a1ac
Myat Soe <mssoelay91@gmail.com>
2023-10-19 15:59:30 +0630

    Falvoring setup

--- lib/app.dart
+++ lib/app.dart
@@ -15,7 +15,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: _flavorBanner(
-        child: MyHomePage(),
+        child: const MyHomePage(),
         show: kDebugMode,
       ),
     );
